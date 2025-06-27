8edc0f6b6d60a22fd2c66a4679023c4261394498
akhnafal-aban <akhnafal03@gmail.com>
2025-06-28 00:27:40 +0700

    Refactor Project model imports and simplify factory method

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Database\Factories\ProjectFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 final class Project extends Model
 {
@@ -64,6 +65,6 @@ final class Project extends Model
 
     public static function newFactory()
     {
-        return \Database\Factories\ProjectFactory::new();
+        return ProjectFactory::new();
     }
 }
